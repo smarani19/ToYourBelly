8a5b3ae2f8f4a576271e4165c961db561026d2fb
smarani19 <titlibas.123@gmail.com>
2023-09-02 23:40:35 +0530

    add files

---
+++ backend/Routes/DisplayData.js
@@ -0,0 +1,15 @@
+const express= require('express')
+const router= express.Router()
+
+router.post('/foodData', (req,res)=>{
+
+    try{
+        res.send([global.food-items, global.categories])
+    }
+    catch(error) {
+        console.error(error.message);
+        res.send("server error")
+    }
+})
+
+module.exports = router; 
